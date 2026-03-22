3e89f0b52240bfc8849b22e23706b40c383553ac
stevenyhf <yhf213@gmail.com>
2026-03-22 12:41:30 +0800

    Add new batch of markdown posts

--- _posts/mdtohtml.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for %%i in (*.md) do (
-    echo Converting %%~ni.md to %%~ni.html
-    pandoc "%%~ni.md" -o "%%~ni.html"
-)
-pause
-
-#pandoc -f markdown -t html article.md -o article.html
